641ae67bcb3e754515116cc3ab0c118ded315926
magnetophys <evgeniy_gordeev@yahoo.com>
2022-07-27 14:16:47 +0300

    Add files via upload

--- SPAM + Atmosphere Photoionization/example_MSISE_CCMC.m
+++ SPAM + Atmosphere Photoionization/example_MSISE_CCMC.m
@@ -1,1 +1,73 @@
+clear all
+
+% Calculation of photoionization rate profiles with the NRLMSISE-00
+% atmosphere results obtained from the Comunity Coordinated Modelling Center 
+% webpage https://ccmc.gsfc.nasa.gov/
+
+% Format of the NRLMSISE-00 file:
+% 1) Height range 79:1:1000 km;
+% 2) Required fields (select when prepare the MSISE output at CCMC) are specified 
+% below in the NPUT section. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%% ----- INPUT -----
+alts = 80:250 ;  % altitude range for Q calculation
+
+% -- MSIS'00 in altitude range [79:1000]km --
+% msis = load('nrlmsise_2009Jun20_10UT.txt') ;  % neutral atmosphere model(F10.7=69 sfu)
+msis = load('nrlmsise_2015Jun18_10UT.txt') ;  % neutral atmosphere model(F10.7=140 sfu)
+Year = msis(1,1) ;               % Year
+Mon = msis(1,2) ;                % Month
+Day = msis(1,3) ;                % Day (of Month)
+Hour = msis(1,4) ;               % Hour (of Day, decimal)
+alt_msis = msis(:,5) ;           % Height, km
+GLat = msis(1,6) ;               % Geographic Latitude range -90:90
+GLon = msis(1,7) ;               % Geographic Longitude range -180:+180
+nO_msis = msis(:,8) .*1e+6 ;     % Concentration of Atomic Oxygen O [cm-3 --> m-3]
+nN2_msis = msis(:,9) .*1e+6 ;    % Nitrogen N2
+nO2_msis = msis(:,10) .*1e+6 ;   % Oxygen O2
+T_msis = msis(:,11) ;            % Neutral Tempereature [K]
+nHe_msis = msis(:,12) .*1e+6 ;   % Helium He
+nAr_msis = msis(:,13) .*1e+6 ;   % Argon Ar
+nH_msis = msis(:,14) .*1e+6 ;    % Hydrogen H
+nN_msis = msis(:,15) .*1e+6 ;    % Nitrogen N
+F107 = msis(1,16) ;              % F10.7 (daily)
+
+% -- NO empirical concentration by [Mirta, ...] --
+nNO_mirta = 0.4*exp(-3700./T_msis).*nO2_msis + 5*1e-7*nO_msis ;  
+
+% -- combined Input --
+INPUT.alts = alts ;      INPUT.Year = Year ;        INPUT.Mon = Mon ;        INPUT.Day = Day ;
+INPUT.Hour = Hour ;      INPUT.alt_msis = alt_msis ;    INPUT.GLat = GLat ;       INPUT.GLon = GLon ;
+INPUT.nO_msis = nO_msis ;   INPUT.nN2_msis = nN2_msis ;   INPUT.nO2_msis = nO2_msis ;  INPUT.T_msis = T_msis ;
+INPUT.nHe_msis = nHe_msis ; INPUT.nAr_msis = nAr_msis ;   INPUT.nH_msis = nH_msis ;   INPUT.nN_msis = nN_msis ;
+INPUT.F107 = F107 ;     INPUT.nNO_mirta = nNO_mirta ;
+% -------------------------------------------------------------------------
+
+
+%% ----- call photoionization function -----
+[qN,qN2,qO,qO2,qNO] = fun_StraightPhotoionization(INPUT) ;
+% -------------------------------------------------------------------------
+
+
+%% ----- plot results in [cm-3 s-1] -----
+figure
+semilogx(qN.*1e-6,alts,'c') ; hold on ; grid on ; % qN
+semilogx(qN2.*1e-6,alts,'r') ;  % qN2
+semilogx(qO.*1e-6,alts,'b') ;  % qO
+semilogx(qO2.*1e-6,alts,'m') ;  % qO2
+semilogx(qNO.*1e-6,alts,'g') ;  % qNO
+semilogx(sum([qN;qN2;qO;qO2;qNO]).*1e-6,alts,'k', 'linewidth', 2) ;  % q total
+xlim([1e+0 1e+4]) ; 
+ylim([alts(1) alts(end)]) ;
+text(15,max(alts)-15,'qN', 'color','c') ;
+text(15,max(alts)-25,'qN_2', 'color','r') ;
+text(15,max(alts)-35,'qO', 'color','b') ;
+text(15,max(alts)-45,'qO_2', 'color','m') ;
+text(15,max(alts)-55,'qNO', 'color','g') ;
+text(15,max(alts)-65,'\Sigmaq', 'color','k') ;
+xlabel('q, cm^-^3s^-^1') ;
+ylabel('Altitude, km') ;
 
---
+++ SPAM + Atmosphere Photoionization/fun_StraightPhotoionization.m
@@ -0,0 +1,135 @@
+function [qN,qN2,qO,qO2,qNO] = fun_StraightPhotoionization(INPUT)
+% fun_StraightPhotoionization.m ::: calculates the photoionization rates Q of 
+% the basic atmospheric gas molecules and atoms (O, O2, N, N2, NO) 
+% due to impact of the straigt solar radiation.
+%
+% More details on the photoionization calculation procedure can be found in
+% [Nikolaeva et al., Atmosphere 2021], and on the SPAM solar spectrum model in
+% [Nikolaeva & Gordeev, GMD 2022]
+%
+% INPUT is a structure array containing all needed input variables
+
+
+%% ----- INPUT -----
+alts = INPUT.alts ;  % altitude range for Q calculation
+
+% -- MSIS'00 in altitude range [79:1000]km --
+Year = INPUT.Year ; 
+Mon = INPUT.Mon ;
+Day = INPUT.Day ;
+Hour = INPUT.Hour ;  
+alt_msis = INPUT.alt_msis ;
+GLat = INPUT.GLat ;  % Geodetic Latitude range -90:90
+GLon = INPUT.GLon ;  % Geodetic Longitude range -180:+180
+nO_msis = INPUT.nO_msis ;   % m-3
+nN2_msis = INPUT.nN2_msis ;
+nO2_msis = INPUT.nO2_msis ;
+T_msis = INPUT.T_msis ;
+nHe_msis = INPUT.nHe_msis ; 
+nAr_msis = INPUT.nAr_msis ;
+nH_msis = INPUT.nH_msis ;
+nN_msis = INPUT.nN_msis ;
+F107 = INPUT.F107 ;
+
+% -- NO empirical concentration by [Mirta, ...] --
+nNO_mirta = INPUT.nNO_mirta ;  
+% -------------------------------------------------------------------------
+
+
+
+%% ----- some universal constants -----
+kB = 1.3805404e-23 ;  % Boltzmann const
+amu = 1.660539e-27 ;  % Unified atomic mass unit
+%--------------------------------------------------------------------------
+
+
+
+%% ----- Aero-SPAM [Nikolaeva and Gordeev, 2022] -----
+tab_ASPAM = load('tab_AeroSPAM.txt') ;
+p1_ASPAM = tab_ASPAM(:,4) ;
+p2_ASPAM = tab_ASPAM(:,5) ;
+p3_ASPAM = tab_ASPAM(:,6) ;
+
+% -- photon flux on the top of atmosphere --
+Itop_UV = p1_ASPAM(1:end-1).*F107^2 + p2_ASPAM(1:end-1).*F107 + p3_ASPAM(1:end-1) ; 
+Itop_La = p1_ASPAM(end).*F107^2 + p2_ASPAM(end).*F107 + p3_ASPAM(end) ;  % Ly-alpha flux
+%--------------------------------------------------------------------------
+
+
+
+%% ----- Ionization Cross Sections [Nikolaeva et al., 2021, Appendix C] -----
+tab_CS = load('tab_CrossSections.txt') ;
+sO_i = tab_CS(:,3) *1e-22 ;  % sigma O -- cross-sect  i -- ionization    [m2]  
+sO_a = tab_CS(:,4) *1e-22 ;   % sigma O -- cross-sect  a -- absorbtion   [m2]
+sN_i = tab_CS(:,5) *1e-22 ;
+sN_a = tab_CS(:,6) *1e-22 ;
+sO2_i = tab_CS(:,7) *1e-22 ;
+sO2_a = tab_CS(:,8) *1e-22 ;
+sN2_i = tab_CS(:,9) *1e-22 ;
+sN2_a = tab_CS(:,10) *1e-22 ;
+sNO_i = tab_CS(:,11) *1e-22 ;
+sNO_a = tab_CS(:,12) *1e-22 ;
+%--------------------------------------------------------------------------
+
+
+
+%% ----- Chapman function calculation -----
+DoY = datenum(Year,Mon,Day)-datenum(Year,0,0) ; % Day of Year
+declin = -23.44*cosd((360/365)*(DoY+10)) ; % solar declination   
+lst = Hour + GLon/15 ;  % Local apparent solar time [nrmlsis-2000]
+sha = 15*(lst-12) ; % solar hour angle
+SZA = 90-asind( sind(GLat)*sind(declin) + cosd(GLat)*cosd(declin)*cosd(sha)) ; % Solar Zenith Angle
+
+Chap = zeros(size(alt_msis)) ;  % Chapman Function
+for k = 1:length(alt_msis) 
+    MolWeights = [4.0026, 15.9994, 28.0134, 31.9988, 39.948, 1.00797, 14.0067, 30.0061] ;  % Molecular Weights [g/mol]
+    NeutralsDens = [nHe_msis(k), nO_msis(k), nN2_msis(k), nO2_msis(k), nAr_msis(k), nH_msis(k), nN_msis(k), nNO_mirta(k)] ;  % concentration of neutrals 
+    sma = sum(NeutralsDens.*MolWeights)/sum(NeutralsDens) ; % specific mass of the air [g/mol]
+    grav = 9.780327*(1+0.0053024*sind(GLat)^2-0.0000058*sind(2*GLat)^2)-3.086*1e-6*alt_msis(k) ; % gravitational acceleration
+    ScH = (kB*T_msis(k))/(sma*amu*grav) ;  % scale height
+    XX = (6371+alt_msis(k))*1e3/ScH ;  
+    AEF = sqrt(XX/2) * abs(cosd(SZA)) ; % Additional Error Function
+    EF = (1.0606963+0.55643831*AEF) / (1.0619896 + 1.7245609*AEF + AEF^2) * (exp(1)^(-AEF^2)) ;  % Error Function
+    if SZA <= 90 
+        Chap(k) = ((pi/2*XX)^0.5) * (exp(1)^(AEF^2)) * EF ;  % Chapman Function
+    elseif SZA > 90
+        Chap(k) = ((2*pi*XX)^0.5) * (sind(SZA)^0.5*exp(XX*(1-sind(SZA)))-0.5*exp(1)^(AEF^2)*EF) ;  
+    end  
+end
+%--------------------------------------------------------------------------
+
+
+
+%% ----- photoionization rates vertical profile -----
+for ka = 1:length(alts) 
+    
+    % -- values at a specified height --
+    f = find(alt_msis==alts(ka)) ; % 
+    alt = alt_msis(f) ;
+    nO = nO_msis(f) ;   % [m-3]
+    nN2 = nN2_msis(f) ;
+    nO2 = nO2_msis(f) ;
+    nN = nN_msis(f) ;
+    nNO = nNO_mirta(f) ;
+    Talt = T_msis(f) ;   % Atm temp at altititude
+
+    % -- Height integrated concentration --
+    f = find(alt_msis>=alt) ;
+    integr_nO = sum(Chap(f).*nO_msis(f).*(alt_msis(f)-alt_msis(f-1)).*1e3) ;  % integral concentration in the above atmospheric column
+    integr_nO2 = sum(Chap(f).*nO2_msis(f).*(alt_msis(f)-alt_msis(f-1)).*1e3) ;
+    integr_nN2 = sum(Chap(f).*nN2_msis(f).*(alt_msis(f)-alt_msis(f-1)).*1e3) ;
+    integr_nN = sum(Chap(f).*nN_msis(f).*(alt_msis(f)-alt_msis(f-1)).*1e3) ;
+    integr_nNO = sum(Chap(f).*nNO_mirta(f).*(alt_msis(f)-alt_msis(f-1)).*1e3) ;
+    clear f ;
+
+    % -- photoionization rates, m-3s-1 --
+    qO(ka) = nO * sum(sO_i .* Itop_UV .* exp(-(sO_a*integr_nO + sO2_a*integr_nO2 + sN2_a*integr_nN2 + sN_a*integr_nN + sNO_a*integr_nNO) )) ;
+    qO2(ka) = nO2 * sum(sO2_i .* Itop_UV .* exp(-(sO_a*integr_nO + sO2_a*integr_nO2 + sN2_a*integr_nN2 + sN_a*integr_nN + sNO_a*integr_nNO) )) ;
+    qN(ka) = nN * sum(sN_i .* Itop_UV .* exp(-(sO_a*integr_nO + sO2_a*integr_nO2 + sN2_a*integr_nN2 + sN_a*integr_nN + sNO_a*integr_nNO) )) ;
+    qN2(ka) = nN2 * sum(sN2_i .* Itop_UV .* exp(-(sO_a*integr_nO + sO2_a*integr_nO2 + sN2_a*integr_nN2 + sN_a*integr_nN + sNO_a*integr_nNO) )) ;
+    qNO_UV(ka) = nNO * sum(sNO_i .* Itop_UV .* exp(-(sO_a*integr_nO + sO2_a*integr_nO2 + sN2_a*integr_nN2 + sN_a*integr_nN + sNO_a*integr_nNO) )) ;
+    qNO_La(ka) = nNO * sum(1.944e-22 .* Itop_La .* exp(-(9e-25*integr_nO2 + 6e-27*integr_nN2 + 2.4e-22*integr_nNO) )) ;
+    qNO(ka) = qNO_UV(ka) + qNO_La(ka) ;
+end
+%--------------------------------------------------------------------------
+
